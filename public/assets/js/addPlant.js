1bc7015f65564bc0bf9ebebc95407b9eb1386585
Gilberto Resendiz <gresendi@uci.edu>
2021-09-21 15:58:32 -0700

    created additional html to add plant, plant is able to recieve an image, plant is then created

--- public/assets/js/addPlant.js
+++ public/assets/js/addPlant.js
@@ -1,5 +1,5 @@
 const { axios } = window
-let moment = require('moment')
+
 // // import initalizeApp from firebase app (version 9)
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-app.js";
 
@@ -29,14 +29,14 @@ document.getElementById('goHome').addEventListener('click', () => {
 })
 
 document.getElementById('goProfile').addEventListener('click', () => {
-  if (localStorage.getItem('token')){
+  if (localStorage.getItem('token')) {
     window.location = '/profile.html'
-  }else{
+  } else {
     window.location = '/login.html'
   }
-    
-  
-  
+
+
+
 })
 
 document.getElementById('logOut').addEventListener('click', () => {
@@ -44,109 +44,11 @@ document.getElementById('logOut').addEventListener('click', () => {
   window.location = '/login.html'
 })
 
-
-document.getElementById('addPost').addEventListener('click', event => {
-
-  addPost++
-  event.preventDefault()
-  if(addPost<=1){
-  let form = document.createElement('div')
-  form.innerHTML = `
-   <form>
-
-        <div class="input-group mb-3 mt-3">
-          <input type="file" class="form-control" accept="image/*" id="photo">
-          <label class="input-group-text" for="photo"></label>
-          <progress id="uploader">0%</progress>
-        </div>
-        <div class="mb-3">
-          <label for="title" class="form-label">Username</label>
-          <input type="text" class="form-control" id="title">
-        </div>
-        <div class="mb-3">
-          <label for="body" class="form-label">Body</label>
-          <textarea  class="form-control" rows = "2" id="body"></textarea>
-        </div>
-        <button id="createPost" type="submit" class="btn btn-primary">Create Post</button>
-      </form>
-  `
-
-  document.getElementById('topContainer').append(form)
-
-  uploadPhoto()
-
-  document.getElementById('createPost').addEventListener('click', event => {
-    event.preventDefault()
-
-    axios.post('/api/posts', {
-      title: document.getElementById('title').value,
-      photo: imgUrl,
-      body: document.getElementById('body').value
-    }, {
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem('token')}`
-      }
-    })
-      .then(({ data: { id, title, photo, body, u: { username } } }) => {
-        const postElem = document.createElement('li')
-        postElem.className = 'd-flex justify-content-between align-items-start mb-2 listItem'
-        postElem.innerHTML = `
-        <div class="ms-2 me-auto">
-          <div class="fw-bold">${title}</div>
-          ${photo}
-          ${body}
-        </div>
-        <span class="badge bg-primary rounded-pill">${username}</span>
-      `
-        document.getElementById('posts').append(postElem)
-        addPost = 0
-        location.reload()
-
-        let imgUrl = ''
-        
-      })
-      .catch(err => console.error(err))
-  })
-  }
-  
-
-
-
-})
-
-function getPosts() {
-  axios.get('/api/posts', {
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem('token')}`
-      }
-  })
-    .then(({ data: posts }) => {
-      console.log(posts)
-      posts.forEach(({ id, title, body, u: { username } }) => {
-        const postElem = document.createElement('li')
-        postElem.className = 'd-flex justify-content-between align-items-start mb-2 listItem'
-        postElem.innerHTML = `
-        <div class="ms-2 me-auto">
-          <div class="fw-bold">${title}</div>
-          ${body}
-        </div>
-        <span class="badge bg-primary rounded-pill">${username}</span>
-      `
-        document.getElementById('posts').append(postElem)
-      })
-    })
-  .catch(err => {
-    console.log(err)
-    window.location = '/login.html'
-  })
-}
-
-
-function isLoggedIn(){
-  if (localStorage.getItem('token')){
+function isLoggedIn() {
+  if (localStorage.getItem('token')) {
     console.log("logged in")
 
-  }else{
+  } else {
     console.log('not logged in')
     let button = document.getElementById('logOut')
     button.innerHTML = `Sign In`
@@ -214,5 +116,59 @@ function uploadPhoto() {
 }
 
 
-getPosts()
+
+
+
+
+document.getElementById('createPlant').addEventListener('click', event => {
+  event.preventDefault()
+  console.log('adding plant')
+
+
+  let intervals = document.getElementById('sel1').value
+  console.log(intervals)
+  axios.post('/api/plants', {
+    officialName: '',
+    nickName: document.getElementById('plantName').value,
+    photo: imgUrl,
+    care: document.getElementById('care').value,
+    lastWatered: Date.now(),
+    nextWatering: moment().add(intervals, 'days').format(),
+    intervals: intervals
+  }, {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem('token')}`
+    }
+
+  }).then(() => {
+    console.log("plant added")
+    window.location = '/profile.html'
+    imgUrl = ' '
+  })
+    .catch(err => console.error(err))
+
+
+})
+
+
+
+
+
+uploadPhoto()
+
+
+
+
+
+
+
+
+
+
+
+
+
 isLoggedIn()
+
+
+
