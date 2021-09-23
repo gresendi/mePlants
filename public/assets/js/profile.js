e693555aec8568af0fcb05a442ead9e122456f87
Gilberto Resendiz <gresendi@uci.edu>
2021-09-23 13:32:47 -0700

    meger resolved

--- public/assets/js/profile.js
+++ public/assets/js/profile.js
@@ -43,6 +43,7 @@ document.getElementById('goPost').addEventListener('click', () => {
 
 document.getElementById('addPlant').addEventListener('click', () => {
   window.location = '/addPlant.html'
+})
 
   // document.getElementById('createPlant').addEventListener('click', event => {
   //   event.preventDefault()
@@ -72,7 +73,6 @@ document.getElementById('addPlant').addEventListener('click', () => {
   //     .catch(err => console.error(err))
 
 
-<<<<<<< HEAD
 // document.getElementById('createPlant').addEventListener('click', event => {
 //   event.preventDefault()
 //   console.log('adding plant')
@@ -103,40 +103,6 @@ document.getElementById('addPlant').addEventListener('click', () => {
 
 // })
 
-||||||| b08293e
-document.getElementById('createPlant').addEventListener('click', event => {
-  event.preventDefault()
-  console.log('adding plant')
-
-
-  let intervals = document.getElementById('sel1').value
-  console.log(intervals)
-  axios.post('/api/plants', {
-    officialName: '',
-    nickName: document.getElementById('plantName').value,
-    photo: imgUrl,
-    care: document.getElementById('care').value,
-    lastWatered: Date.now(),
-    nextWatering: moment().add(intervals, 'days').format(),
-    intervals: intervals
-  }, {
-    headers: {
-      Authorization: `Bearer ${localStorage.getItem('token')}`
-    }
-
-  }).then(() => {
-    console.log("plant added")
-    window.location = '/profile.html'
-    imgUrl = ' '
-  })
-    .catch(err => console.error(err))
-
-
-})
-
-=======
-  })
->>>>>>> a5905ecbc38d8fd433b2d8f6f1630b97d6842aff
 
 
 
@@ -171,7 +137,6 @@ document.getElementById('createPlant').addEventListener('click', event => {
 
   // })
 
-<<<<<<< HEAD
 // document.getElementById('goProfile').addEventListener('click', () => {
 //   window.location = '/profile.html'
 // })
@@ -197,34 +162,6 @@ document.getElementById('addPlant').addEventListener("click", event => {
   window.location = '/addPlant.html'
 
 })
-||||||| b08293e
-// document.getElementById('goProfile').addEventListener('click', () => {
-//   window.location = '/profile.html'
-// })
-
-document.getElementById('logOut').addEventListener('click', () => {
-  localStorage.removeItem('token')
-  window.location = '/login.html'
-})
-
-document.addEventListener('click', event => {
-  if (event.target.classList.contains('deletePost')) {
-    axios.delete(`/api/posts/${event.target.dataset.id}`, {
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem('token')}`
-      }
-    })
-      .then(() => event.target.parentNode.remove())
-      .catch(err => console.error(err))
-  }
-})
-
-// document.getElementById('addPlant').addEventListener("click", event => {
-//   window.location = '/addPlant.html'
-
-// })
-=======
->>>>>>> a5905ecbc38d8fd433b2d8f6f1630b97d6842aff
 
   document.addEventListener('click', event => {
     event.preventDefault()
@@ -418,60 +355,4 @@ document.addEventListener('click', event => {
     }
   })
 
-  document.getElementById('photo').addEventListener('change', event => {
-
-    console.log('log event');
-
-    // Selected File Image is the event target files
-    let selectedImgFile = event.target.files[0]
-    console.log(selectedImgFile);
-
-    // Create a unique file name to pass the reference
-    let fileName = 'mePlant' + Date.now() + '.png'
-
-    // Assign a metadata (which will show as image/jpeg in storage)
-    let metadata = { contentType: 'image/jpeg' }
-
-    // Create reference to the firebase app storage
-    let storage = getStorage(firebaseApp)
-    // Create reference to storage images/ folder and add the unique file name for the images ref
-    let imagesRef = ref(storage, 'images/' + fileName)
-    // upload to the storage images folder the selected image file and show the metadata
-    let uploadTask = uploadBytesResumable(imagesRef, selectedImgFile, metadata)
-
-    // Register three observers:
-    // 1. 'state_changed' observer, called any time the state changes
-    // 2. Error observer, called on failure
-    // 3. Completion observer, called on successful completion
-    uploadTask.on('state_changed',
-      (snapshot) => {
-        // Observe state change events such as progress, pause, and resume
-        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        console.log('Upload is ' + progress + '% done');
-        let uploader = document.getElementById('uploader')
-        uploader.value = progress
-
-        switch (snapshot.state) {
-          case 'paused':
-            console.log('Upload is paused');
-            break;
-          case 'running':
-            console.log('Upload is running');
-            break;
-        }
-      },
-      (error) => {
-        // Handle unsuccessful uploads
-        console.log(err)
-      },
-      () => {
-        // Handle successful uploads on complete
-        // get the download URL: https://firebasestorage.googleapis.com/...
-        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-          console.log('File available at', downloadURL);
-          imgUrl = downloadURL
-        });
-      }
-    );
-  })
+  
