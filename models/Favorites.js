5a740a0b5267ec32f76ab6c025f40a5294711e6e
Gilberto Resendiz <gresendi@uci.edu>
2021-09-23 10:40:24 -0700

    fixed modal reroute issue

---
+++ models/Favorites.js
@@ -0,0 +1,12 @@
+// const { Model, DataTypes } = require('sequelize')
+// const sequelize = require('../db')
+
+// class Favorites extends Model { }
+
+// Favorites.init({
+//   title: DataTypes.STRING,
+//   photo: DataTypes.STRING,
+//   body: DataTypes.STRING,
+// }, { sequelize, modelName: 'post' })
+
+// module.exports = Post
--- models/Post.js
+++ models/Post.js
@@ -6,7 +6,7 @@ class Post extends Model { }
 Post.init({
   title: DataTypes.STRING,
   photo: DataTypes.STRING,
-  body: DataTypes.STRING
+  body: DataTypes.STRING,
 }, { sequelize, modelName: 'post' })
 
 module.exports = Post
