51a048ef66411ba56f304d0cf850c4fca8b5d2b0
SHARP <ssharp9999@gmail.com>
2021-09-24 11:28:04 -0700

    comments

--- models/index.js
+++ models/index.js
@@ -1,11 +1,10 @@
+// require models
 const User = require('./User.js')
 const Post = require('./Post.js')
 const Plant = require('./Plant.js')
 const Favorite = require('./Favorite.js')
 
-
-
-
+// create connections between tables
 User.hasMany(Post, { foreignKey: 'uid' })
 Post.belongsTo(User, { as: 'u', foreignKey: 'uid' })
 
@@ -15,6 +14,6 @@ Plant.belongsTo(User, {as:'u',foreignKey: 'uid'})
 User.hasMany(Post, { foreignKey: 'uid' })
 Favorite.belongsTo(User, { as: 'u', foreignKey: 'uid' })
 
-
+// export modules
 module.exports = { User, Post, Plant, Favorite }
 
